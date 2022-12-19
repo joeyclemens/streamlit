0c8aebbac85663248ffe8c2b3347817e659fe3b1
Joey <joeyclemens87@gmail.com>
2022-12-19 16:19:09 +0000

    f

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,3 +1,7 @@
+<html>
+<body>
+<h1>hello</h1>
+<py-script>
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -40,3 +44,7 @@ hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
+
+ </py-script>
+  </body>
+</html>
